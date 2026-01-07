dfbe79b22fe87b779e87f7d116a951ad2c10ad21
Konstantinos Spyrou <spyroukon@gmail.com>
2026-01-07 17:54:57 +0200

    feat(security): Introduces csrf protection

--- services/messaging-websocket.service.ts
+++ services/messaging-websocket.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
 import {MessagingSystemService} from "./messaging-system.service";
 import * as UIkit from 'uikit';
+import {HttpClient} from "@angular/common/http";
+import {XsrfTokenExtractor} from "../../survey-tool/catalogue-ui/services/xsrf-token-extractor.service";
 
 declare var SockJS;
 declare var Stomp;
@@ -14,20 +16,26 @@ export class MessagingWebsocketService {
   stompClientUnread: Promise<typeof Stomp> = null;
   stompClientNotification: Promise<typeof Stomp> = null;
 
-  constructor(private messagingService: MessagingSystemService) {}
+  count = 0;
+  topic
+
+  constructor(private messagingService: MessagingSystemService, public http: HttpClient, private xsrf: XsrfTokenExtractor) {
+  }
 
   initializeWebSocketConnectionUnread(topic: string) {
     const ws = new SockJS(URL);
     const that = this;
+    this.topic = topic;
 
     this.stompClientUnread = new Promise((resolve, reject) => {
       let stomp = Stomp.over(ws);
 
       stomp.debug = null; // removes debug logs
-      stomp.connect({}, function(frame) {
+      stomp.connect(this.xsrf.getHeader(), function (frame) {
         const timer = setInterval(() => {
           if (stomp.connected) {
             clearInterval(timer);
+            that.count = 0;
             stomp.subscribe(`${topic}`, (message) => {
               if (message.body) {
                 that.messagingService.unreadMessages.next(JSON.parse(message.body))
@@ -36,9 +44,14 @@ export class MessagingWebsocketService {
             });
             resolve(stomp);
           }
-        }, 500);
+        }, 1000);
       }, function (error) {
-        setTimeout( () => {that.initializeWebSocketConnectionUnread(topic)}, 2000);
+        let timeout = 1000;
+        that.count > 20 ? timeout = 10000 : that.count++ ;
+        setTimeout( () => {
+          // stomp.close();
+          that.initializeWebSocketConnectionUnread(that.topic);
+        }, timeout);
         console.log('STOMP: Reconnecting...');
       });
     });
@@ -57,7 +70,7 @@ export class MessagingWebsocketService {
       let stomp = Stomp.over(ws);
 
       stomp.debug = null; // removes debug logs
-      stomp.connect({}, function(frame) {
+      stomp.connect(this.xsrf.getHeader(), function (frame) {
         const timer = setInterval(() => {
           if (stomp.connected) {
             clearInterval(timer);
@@ -76,7 +89,12 @@ export class MessagingWebsocketService {
           }
         }, 500);
       }, function (error) {
-        setTimeout( () => {that.initializeWebSocketConnectionNotification(topic)}, 2000);
+        let timeout = 1000;
+        that.count > 20 ? timeout = 10000 : that.count++ ;
+        setTimeout( () => {
+          // stomp.close();
+          that.initializeWebSocketConnectionNotification(that.topic);
+        }, timeout);
         console.log('STOMP: Reconnecting...');
       });
     });
